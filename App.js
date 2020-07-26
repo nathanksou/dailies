47015479e6b8d1976242afdf1b7277895f48ba55
Nathan Sou <nathanksou@gmail.com>
2020-07-25 21:25:58 -0700

    Implement Weather component

--- App.js
+++ App.js
@@ -1,24 +1,18 @@
-import React, { useState }from 'react';
-import { Alert, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+import Weather from './components/Weather.js';
 
 const App = () => {
-  const [location, setLocation] = useState(null);
-  findCoordinates = () => {
-    navigator.geolocation.getCurrentPosition(
-      position => {
-        const coordinates = JSON.stringify(position);
-        setLocation(coordinates);
-      },
-      error => Alert.alert(error.message),
-      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
-    );
-  };
+  const [isLoading, setIsLoading] = useState(false);
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={findCoordinates}>
-        <Text style={styles.welcome}>Find My Coords?</Text>
-        <Text>Location: {location}</Text>
-      </TouchableOpacity>
+      {isLoading ? (
+        <Text>Fetching The Weather</Text>
+      ) : (
+        // <Text>Minimalist The Weather</Text>
+        <Weather />
+      )}
     </View>
   );
 };
@@ -26,14 +20,7 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
+    backgroundColor: '#000',
   }
 });
 
